46e142a69595cf5f71cbc09d22f5cf1ded8999b5
milliax <wei@sivir.pw>
2023-05-17 04:24:38 +0800

    working on automate cowo

--- include/auto_cow.h
+++ include/auto_cow.h
@@ -1,20 +1,26 @@
 #include <utility>
 #include <vector>
 
+#include "cows_and_bulls.h"
+
 struct Guess {
     int guess[4];
     std::pair<int, int> result;
 };
 
 class AutoCow {
+    CowsAndBulls cb;
+    short ans_appeared;
     bool possibility_map[4][10];
-    std::pair<int,int> guess(int[4]);
+
     int* determine();
     void init();
+    void save_guess(int*, std::pair<int, int>*);
+    void save_status();
+    std::pair<int, int> guess(int[4]);
     std::vector<Guess> guess_history;
 
    public:
     AutoCow() = default;
-
     void start();
 };
